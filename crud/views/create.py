179cc8402ab5ab96bc5d69a0d12927b9a28388ee
yuuta3594 <yuuta3594@outlook.jp>
2019-12-14 02:22:13 +0900

    Update create.py

--- crud/views/create.py
+++ crud/views/create.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.views.generic import CreateView
 from crud.forms import CRUDForm
 from crud.models import CRUD
@@ -7,3 +8,6 @@ class CRUDCreateView(CreateView):
     model = CRUD
     form_class = CRUDForm
     template_name = 'crud/create.html'
+
+    def get_success_url(self):
+        return reverse('detail', kwargs={'pk': self.object.id})
