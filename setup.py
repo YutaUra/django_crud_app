2b1e294395aa17363dae391edf2db03d4f855b30
yuuta3594 <yuuta3594@outlook.jp>
2019-12-17 12:45:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-crud-app',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(exclude=('django_crud_app',)),
     tall_requires=['django', ],
     include_package_data=True,
